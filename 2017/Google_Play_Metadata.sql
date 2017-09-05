262d4deba9a6f8f02f53950506ad7f770cbe6391
tongtzeho <tangzihao@pku.edu.cn>
2017-09-05 16:21:00 +0800

    Create Google_Play_Metadata.sql

---
+++ 2017/Google_Play_Metadata.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Metadata (
+	Package_Name varchar(180) not null,
+	Time bigint not null,
+	Name varchar(100),
+	Download bigint,
+	Rating float,
+	Rating_Num bigint,
+	Five_Star bigint,
+	Four_Star bigint,
+	Three_Star bigint,
+	Two_Star bigint,
+	One_Star bigint,
+	Category varchar(40),
+	Tag varchar(120),
+	Edition varchar(30),
+	Price varchar(20),
+	Update_Time bigint,
+	Developer varchar(60),
+	Description varchar(5000),
+	Release_Note varchar(1500),
+	Similar_Apps text,
+	PRIMARY KEY (Package_Name)
+);
